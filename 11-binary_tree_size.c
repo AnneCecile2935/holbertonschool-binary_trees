920b2e94e9b15fcdb1e86c2b32c6ab155df69a9c
Annece2935 <anne-cecile.colleter@live.fr>
2025-04-07 14:19:36 +0200

    update task 11 good

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -8,5 +8,5 @@ size_t binary_tree_size(const binary_tree_t *tree)
 {
 if (tree == NULL)
 return (0);
-return (1 + binary_tree_size(tree->left + binary_tre__size(tree->right)));
+return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
 }
