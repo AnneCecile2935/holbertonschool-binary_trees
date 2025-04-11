17601df0a69f481dc9ddc591c6f028bbd6fc33a8
Annece2935 <anne-cecile.colleter@live.fr>
2025-04-11 18:32:14 +0200

    update task 100

---
+++ 100-binary_trees_ancestor.c
@@ -0,0 +1,12 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_uncle - function that find the uncle of a node
+ * @node: is a pointer to the node to find the uncle
+ * Return: the uncle
+ */
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
+{
+	if (first == NULL || second == NULL)
+	return (NULL);
+	
+}
